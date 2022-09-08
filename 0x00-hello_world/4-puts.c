5272b91209e12ff1ed0eaad350d76d09c285f6ce
Lucas0316 <hrmhrm087@gmail.com>
2022-09-08 10:48:58 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+	//string initialisation
+
+    char Mystr[] = "Programming is like building a multilingual puzzle";
+
+    
+
+    puts(Mystr); //writing the string to stdout
+
+    
+
+    return 0;
+
+}
+
