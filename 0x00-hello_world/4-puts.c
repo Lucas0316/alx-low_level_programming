0336623a8f7a5fc91b2ce3078a1a61cd6536c243
Lucas0316 <hrmhrm087@gmail.com>
2022-09-08 11:05:07 -0700

    hello using puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* The program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
 {
-	//string initialisation
-    char Mystr[] = "Programming is like building a multilingual puzzle";
-
-    puts(Mystr); //writing the string to stdout
-
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
