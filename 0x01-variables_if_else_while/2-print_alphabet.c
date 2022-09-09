6f3ec773e03aa7e04cb4bbb2427ef0695b6314c3
Lucas0316 <hrmhrm087@gmail.com>
2022-09-09 11:27:46 -0700

    fix 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,9 +12,8 @@ int main(void)
 {
 	char low;
 	
-	for (low = 'a'; low <= 'z'; low++) {
-		putchar(low);
-		putchar('\n');
+	for (low = 'a'; low <= 'z'; low++)
+	{	putchar(low);
 }
 	return (0);
 }
