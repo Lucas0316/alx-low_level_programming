52f762293a0a3bf2efd645eeb061ae87a431bae3
Lucas0316 <hrmhrm087@gmail.com>
2022-09-08 19:55:02 -0700

    data types fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	int a;
-	char b;
-	float c;
-	long int d;
-	long long int e;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("Size of int: %zu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of char: %zu byte(s)\n", (unsigned long)sizeof(b));
-        printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(c));
-        printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(d));
-        printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(e));
-        return (0);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }	
