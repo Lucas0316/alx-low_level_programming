cde231b0d667f098bbb7ce30eb3033e4385f6b34
Lucas0316 <hrmhrm087@gmail.com>
2022-09-09 14:54:47 -0700

    t456

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * description; the function returns all single digit numbersfrom 0 to 9
+ * 
  * Return : Always 0 (Success)
  */
+
 int main(void)
 {
 	int c;
