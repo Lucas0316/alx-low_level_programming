92c6cb733d7518a2d6bad541dc2e8814d3c9c5bf
Lucas0316 <hrmhrm087@gmail.com>
2022-09-08 20:25:10 -0700

    using the write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,6 +7,6 @@ include <stdio.h>
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59)
 	return (1);
 }
