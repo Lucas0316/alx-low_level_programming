c9e571d031faf3957880b62ee09008a699452bf5
Lucas0316 <hrmhrm087@gmail.com>
2022-09-20 17:24:19 -0700

    fix 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _puts - prints a strig to stdout
+* Return: Nothing
+*/
+
+int main(void)
+{
+char *str;
+
+str = "Fear is the enemy of all dreams";
+puts (string);
+
+Return (0);
+}
